a617db831b9a8adce9970400c4e138cead8cace1
Alex Blenkinsop <Alex.blenkinsop@hotmail.co.uk>
2024-04-19 12:17:44 +0100

    save patristic distances

--- scripts/extract-patristic-distances-from-trees.R
+++ scripts/extract-patristic-distances-from-trees.R
@@ -3,14 +3,41 @@ require(ape)
 require(adephylo)
 require(ggplot2)
 require(data.table)
+library(readr)
 
-out.dir <- 'data_Ams/analysis_220713/trees'
-trsm <-'AmsMSM'
+args <- list(
+  in.dir = '/rds/general/project/ratmann_roadmap_data_analysis/live/analysis_220713',
+  out.dir = '/rds/general/project/ratmann_roadmap_data_analysis/live/transmission_sources/patristic_distances',
+  #out.dir = 'data_Ams/analysis_220713/trees'
+  trsm = 'AmsMSM',
+  REP = '000'
+)
 
-infiles <- data.table(FIN=list.files(out.dir, pattern='\\.newick$',full.names=TRUE))
+## command line parsing if any
+args_line <-  as.list(commandArgs(trailingOnly=TRUE))
+if(length(args_line) > 0)
+{
+  stopifnot(args_line[[1]]=='-in.dir')
+  stopifnot(args_line[[3]]=='-out.dir')
+  stopifnot(args_line[[5]]=='-trsm')
+  stopifnot(args_line[[7]]=='-REP')
+
+  args <- list()
+  args[['in.dir']] <- args_line[[2]]
+  args[['out.dir']] <- args_line[[4]]
+  args[['trsm']] <- args_line[[6]]
+  args[['REP']] <- args_line[[8]]
+}
+args
+
+out.dir <- args$out.dir
+
+infiles <- data.table(FIN=list.files(c(file.path(args$in.dir,'phyloscanner'),
+                                       file.path(args$in.dir,'phyloscanner_B')), pattern='\\.newick$',full.names=TRUE))
 infiles[, ST:= gsub('.*_subtype_([A-Za-z0-9]+)_.*','\\1',basename(FIN))]
 infiles[, SELECT:= gsub('^.*_rerooted_([A-Za-z]+)\\.newick$','\\1', basename(FIN))]
-infiles <- subset(infiles,SELECT==trsm) # we only need for one trsm gp
+infiles[, REP:= gsub('^.*_wOutgroup_([0-9]+)_.*','\\1', basename(FIN))]
+infiles <- subset(infiles,SELECT==args$trsm & REP==args$REP) # we only need for one trsm gp
 
 for(s in 1:nrow(infiles)){
   tree <- read.tree(infiles[s,FIN])
@@ -21,8 +48,8 @@ for(s in 1:nrow(infiles)){
   distance <- distTips(tree)
   # Transform to a matrix
   distance_matrix <- as.matrix(distance)
-  saveRDS(distance,file=file.path(out.dir, paste0('distance_',infiles[s,SELECT],'_',infiles[s,ST],'.rds')))
-  saveRDS(distance_matrix,file=file.path(out.dir, paste0('distance_matrix_',infiles[s,SELECT],'_',infiles[s,ST],'.rds')))
+  saveRDS(distance,file=file.path(out.dir, paste0('distance_',infiles[s,SELECT],'_',infiles[s,ST],'_',infiles[s,REP],'.rds')))
+  saveRDS(distance_matrix,file=file.path(out.dir, paste0('distance_matrix_',infiles[s,SELECT],'_',infiles[s,ST],'_',infiles[s,REP],'.rds')))
 
   gen_dist_tab <- reshape2::melt(distance_matrix)
   gen_dist_tab <- as.data.table(gen_dist_tab)
@@ -33,23 +60,23 @@ for(s in 1:nrow(infiles)){
   # Add subtype in both tables
   gen_dist_tab[,subtype:=infiles[s,ST]]
 
-  saveRDS(gen_dist_tab,file=file.path(out.dir, paste0('pairwise_dist_',infiles[s,SELECT],'_',infiles[s,ST],'.rds')))
+  saveRDS(gen_dist_tab,file=file.path(out.dir, paste0('pairwise_dist_',infiles[s,SELECT],'_',infiles[s,ST],'_',infiles[s,REP],'.rds')))
 }
 
 gen_dist_tab <- list()
 for(s in c('Bc1','Bc2','Bc3','Bc4')){
-  gen_dist_tab[[s]] <- readRDS(file=file.path(out.dir, paste0('pairwise_dist_',trsm,'_',s,'.rds')))
+  gen_dist_tab[[s]] <- readRDS(file=file.path(args$out.dir, paste0('pairwise_dist_',args$trsm,'_',s,'_',args$REP,'.rds')))
 }
 gen_dist_tabB <- do.call(`rbind`,gen_dist_tab)
 gen_dist_tabB <- as.data.table(gen_dist_tabB)
-saveRDS(gen_dist_tabB,file=file.path(out.dir, paste0('pairwise_dist_',trsm,'_B.rds')))
+saveRDS(gen_dist_tabB,file=file.path(out.dir, paste0('pairwise_dist_',args$trsm,'_B','_',args$REP,'.rds')))
 
 subtypes <- unique(infiles$ST)
 subtypes <- subtypes[!grepl('Bc',subtypes)]
 subtypes <- c(subtypes,'B')
 gen_dist_tab <- list()
 for(s in subtypes){
-  gen_dist_tab[[s]] <- readRDS(file=file.path(out.dir, paste0('pairwise_dist_',trsm,'_',s,'.rds')))
+  gen_dist_tab[[s]] <- readRDS(file=file.path(out.dir, paste0('pairwise_dist_',args$trsm,'_',s,'_',args$REP,'.rds')))
 }
 # Create a large data table for both trees
 
@@ -61,7 +88,6 @@ gen_dist <- gen_dist[grep('Amst',gen_dist$Var1),]
 gen_dist <- gen_dist[grep('Amst',gen_dist$Var2),]
 
 # Extract the sequences ID from the tree labels
-library(readr)
 regex.tip.label <- '^([A-Za-z]+)_+([0-9]+)_([0-9-]+)_(T[0-9]+)$'
 regex.tip.label2 <- '^([A-Za-z]+)_+(T[0-9]+)_([0-9]+)_([a-zA-Z-]+)_([a-zA-Z0-9-]+)_([a-zA-Z]+)-([a-zA-Z]+)_([a-zA-Z_]+)_([0-9-]+)_([0-9]+)$'
 gen_dist[, Var1_id:= as.numeric(gsub(regex.tip.label,'\\2',Var1))]
@@ -77,5 +103,5 @@ setnames(gen_dist,c("Var1","Var2"),c("FROM_SEQUENCE_ID","TO_SEQUENCE_ID"))
 setnames(gen_dist,"value","distance")
 # Remove duplicate pairs
 gen_dist <- unique(gen_dist,by= c("FROM_SEQUENCE_ID","TO_SEQUENCE_ID"))
-saveRDS(gen_dist,file=file.path(out.dir, paste0('pairwise_dist_allSTs_',trsm,'.rds')))
+saveRDS(gen_dist,file=file.path(out.dir, paste0('pairwise_dist_allSTs_',args$trsm,'_',args$REP,'.rds')))
 
---
+++ scripts/using_published_data/submit-job-extract-patristic-distances.R
@@ -0,0 +1,104 @@
+require(data.table)
+require(stringr)
+
+#	function to make PBS header
+make.PBS.header <- function(hpc.walltime=23, hpc.select=1, hpc.nproc=8, hpc.mem= "80gb", hpc.load= "module load anaconda3/personal\nsource activate src", hpc.q=NA, hpc.array=1 )
+{
+  pbshead <- "#!/bin/sh"
+  tmp <- paste("#PBS -l walltime=", hpc.walltime, ":59:00", sep = "")
+  pbshead <- paste(pbshead, tmp, sep = "\n")
+  tmp <- paste("#PBS -l select=", hpc.select, ":ncpus=", hpc.nproc,":ompthreads=", hpc.nproc,":mem=", hpc.mem, sep = "")
+  pbshead <- paste(pbshead, tmp, sep = "\n")
+  pbshead <- paste(pbshead, "#PBS -j oe", sep = "\n")
+  if(hpc.array>1)
+  {
+    pbshead	<- paste(pbshead, "\n#PBS -J 1-", hpc.array, sep='')
+  }
+  if(!is.na(hpc.q))
+  {
+    pbshead <- paste(pbshead, paste("#PBS -q", hpc.q), sep = "\n")
+  }
+  pbshead	<- paste(pbshead, hpc.load, sep = "\n")
+  pbshead
+}
+
+# input args
+if(1)
+{
+  hpc.nproc.cmdstan <- 5
+  args <- data.table(
+    source_dir = '~/git/transmission.flows.by.birthplace',
+    in.dir = '/rds/general/project/ratmann_roadmap_data_analysis/live/analysis_220713',
+    out.dir = '/rds/general/project/ratmann_roadmap_data_analysis/live/transmission_sources/patristic_distances',
+    #out.dir = 'data_Ams/analysis_220713/trees'
+    trsm = 'AmsMSM'#,
+    #REP = '000'
+  )
+  out.dir <- args$out.dir
+}
+
+if(1)
+{
+  tmp <- data.table(REP=0:2)
+  tmp[, REP:= as.character(str_pad(REP, 3, pad = "0"))]
+  set(args, NULL, colnames(tmp), NULL)
+  tmp[, dummy:= 1L]
+  args[, dummy:= 1L]
+  args <- merge(args, tmp, by='dummy')
+  set(args, NULL, 'dummy', NULL)
+}
+
+# make commands
+cmds <- vector('list', nrow(args))
+for(i in seq_len(nrow(args)))
+{
+  #	general housekeeping
+  # first generate pairs
+  cmd    <- ''
+  #	general housekeeping
+  cmd    <- paste0(cmd,"CWD=$(pwd)\n")
+  cmd    <- paste0(cmd,"echo $CWD\n")
+  tmpdir.prefix	<- paste0('src_',format(Sys.time(),"%y-%m-%d-%H-%M-%S"))
+  tmpdir    <- paste0("$CWD/",tmpdir.prefix)
+  cmd    	<- paste0(cmd,"mkdir -p ",tmpdir,'\n')
+  #	generate data set and run if not using cmdstan
+  cmd     <- paste0( cmd, 'echo "----------- Generating input data: ------------"\n')
+  tmp     <- paste0('Rscript ', file.path(args$source_dir[i],args$script_make_pairs[i]),
+                    ' -in.dir ', args$in.dir[i],'',
+                    ' -out.dir ', tmpdir,'',
+                    ' -trsm ', args$trsm[i],'',
+                    ' -REP ', args$REP[i],''
+  )
+  cmd    	<- paste0(cmd, tmp, '\n')
+
+  #	general housekeeping
+  cmd 	<- paste0( cmd, 'echo "----------- Copy files to out directory: ------------"\n')
+  cmd		<- paste0(cmd, 'cp -R --no-preserve=mode,ownership "', tmpdir,'"/* ', out.dir,'\n')
+  cmd		<- paste0(cmd, 'chmod -R g+rw ', out.dir,'\n')
+
+  cmds[[i]]	<- cmd
+}
+
+pbshead <- make.PBS.header(hpc.walltime=23,
+                            hpc.select=1,
+                            hpc.nproc=1,
+                            hpc.mem= "30gb",
+                            hpc.load= "module load anaconda3/personal\nsource activate phylo",
+                            hpc.q=NA,
+                            hpc.array= length(cmds) )
+
+#	make array job
+for(i in seq_len(nrow(args)))
+{
+  cmds[[i]] <- paste0(i,')\n',cmds[[i]],';;\n')
+}
+cmd		<- paste0('case $PBS_ARRAY_INDEX in\n',paste0(cmds, collapse=''),'esac')
+cmd		<- paste(pbshead,cmd,sep='\n')
+
+#	submit job
+outfile		<- gsub(':','',paste("pds",paste(strsplit(date(),split=' ')[[1]],collapse='_',sep=''),'sh',sep='.'))
+outfile		<- file.path(args$out.dir[1], outfile)
+cat(cmd, file=outfile)
+cmd 		<- paste("qsub", outfile)
+cat(cmd)
+cat(system(cmd, intern= TRUE))
