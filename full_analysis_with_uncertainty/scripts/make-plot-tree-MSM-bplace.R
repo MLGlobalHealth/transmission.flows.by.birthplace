1a248da2aa16fdf257fa970be1b827f44d2ef869
Alex Blenkinsop <Alex.blenkinsop@hotmail.co.uk>
2024-05-20 10:57:14 +0100

    phylogeny for cover

--- full_analysis_with_uncertainty/scripts/make-plot-tree-MSM-bplace.R
+++ full_analysis_with_uncertainty/scripts/make-plot-tree-MSM-bplace.R
@@ -75,8 +75,6 @@ pairs <- readRDS(file.path(pairs.dir,paste0(trsm,"_pairs.rds")))
   setnames(tmp,'TO_CLUSTER_NUMBER','FROM_CLUSTER_NUMBER')
   d1 <- merge(d1,tmp,by='FROM_CLUSTER_NUMBER',all.x=T)
   d1[is.na(poss_src), poss_src:= 0]
-  #d1[poss_src==T, poss_src_cat:=prs]
-  #d1[is.na(poss_src_cat), poss_src_cat:=4]
 
   x <- full_join(as_tibble(tree), d1, by = c("label" = "tip"))
   tree2 <- as.treedata(x)
@@ -93,9 +91,6 @@ pairs <- readRDS(file.path(pairs.dir,paste0(trsm,"_pairs.rds")))
   new.individual <- factor(new.individual, levels=c(0,1,2,3),
                            labels=c('Non-Amsterdam MSM','Amsterdam MSM',
                                     'Amsterdam MSM - potential source' ,'Amsterdam MSM - infected 2010-2021'))
-  #border <- tree2@data$poss_src
-  #border <- factor(border, levels=c(0,1),
-  #                 labels=c('Not a recipient or possible source','Recipient or possible source'))
   border <- tree2@data$bplace
   border <- factor(border, levels=c(0,1,2),
                            labels=c('Non-Amsterdam','Born in the Netherlands','Born outside the Netherlands'))
@@ -106,15 +101,8 @@ pairs <- readRDS(file.path(pairs.dir,paste0(trsm,"_pairs.rds")))
     pal_branch <- c(pal[3],'grey50')
 
     tips <- tree$tip.label %>% length()
-    #tree.display <- ggtree(tree, color='grey50', size = 0.1) + #%<+% d1 +
       tree.display <- ggtree(tree, aes(color=new.branch.colours), size = 0.3) + #%<+% d1 +
-        #scale_color_manual(values = pal_branch, labels=c('Amsterdam MSM subgraphs',
-        #                                                 'Amsterdam non-MSM or\noutside Amsterdam'),
-        #                   na.value = "black", drop = F, name = "") +
-        #geom_point2(aes(color=new.individual), size=0.66) +
       geom_tippoint(aes(color=new.individual,shape=border), size=2) + # 0.75
-      #geom_tiplab(size=0.2) +
-      #geom_tippoint(aes(color=FROM_AGE_GP), size=0.66) +
       geom_treescale(x=-0.01, y = length(tree$tip.label)*0.5,  offset=4) +
         scale_shape_manual(values = c("Non-Amsterdam" = 20,
                                       "Born in the Netherlands" = 15,
@@ -136,11 +124,6 @@ pairs <- readRDS(file.path(pairs.dir,paste0(trsm,"_pairs.rds")))
                                    "Non-Amsterdam MSM" = 'Other individuals from outside Amsterdam or a different Amsterdam risk group',
                                    'NA' = ''),
                           na.value = "black", drop = F, name = "") +
-      #scale_colour_manual(values = huh2, labels=c('Non-Amsterdam MSM',
-      #                                                          'Amsterdam MSM',
-      #                                                          'Amsterdam MSM - potential source',
-      #                                                          'Amsterdam MSM - infected 2010-2021'),
-      #                    na.value = "black", drop = F, name = "") +
                           theme(legend.text = element_text(size=11),legend.position='none') +
       guides(col=guide_legend(ncol=1)) +
       ylim(-1, length(tree$tip.label) +1)
@@ -149,7 +132,6 @@ pairs <- readRDS(file.path(pairs.dir,paste0(trsm,"_pairs.rds")))
 
 ## change borders
     tips <- tree$tip.label %>% length()
-    #tree.display <- ggtree(tree, color='grey50', size = 0.1) + #%<+% d1 +
     tree.display <- ggtree(tree, aes(color=new.branch.colours), size = 0.2, layout='fan') + #%<+% d1 +
       geom_tippoint(aes(color=new.individual,shape=border), size=0.9) +
       geom_treescale(x=-0.01, y = length(tree$tip.label)*0.5,  offset=4) +
@@ -179,33 +161,35 @@ pairs <- readRDS(file.path(pairs.dir,paste0(trsm,"_pairs.rds")))
 
     ggsave(file.path(outdir,glue("type_{subtype}_tree_newlabels_{trsm}_coloursubgraphs_tips_borders_bplace_fan.pdf")), width = 10, height = tips*0.02, limitsize = F)
 
+# plot with thicker lines and points for cover art
+    ggtree(tree, aes(color=new.branch.colours,size=new.branch.colours), layout='fan') + #%<+% d1 +
+      geom_tippoint(aes(color=new.individual,shape=border), size=1.4) +
+      geom_treescale(x=-0.01, y = length(tree$tip.label)*0.5,  offset=4) +
+      scale_size_manual(values = c("Amsterdam non-MSM or\noutside Amsterdam" = 0.2,
+                                   "Amsterdam MSM subgraphs" = 0.4),
+                        na.value=0.2) +
+      scale_shape_manual(values = c("Non-Amsterdam" = 20,
+                                    "Born in the Netherlands" = 15,
+                                    "Born outside the Netherlands" = 17),
+                         labels = c("Non-Amsterdam" = "Non-Amsterdam",
+                                    "Born in the Netherlands" = "Born in the Netherlands",
+                                    "Born outside the Netherlands" = "Born outside the Netherlands"),
+                         drop = F, name = "") +
+      scale_colour_manual(values = c("Amsterdam MSM subgraphs" = pal[3],
+                                     "Amsterdam MSM - infected 2010-2021"= pal[1],
+                                     "Amsterdam MSM - potential source" = pal[2],
+                                     "Amsterdam MSM" = pal[4],
+                                     "Non-Amsterdam MSM" = 'grey50',
+                                     'NA' = 'grey50'),
+                          labels=c("Amsterdam MSM subgraphs" = 'Amsterdam MSM subgraphs',
+                                   "Amsterdam MSM - infected 2010-2021" = 'Amsterdam MSM infected in 2010-2021',
+                                   "Amsterdam MSM - potential source" = 'Plausible source of an Amsterdam MSM infection in 2010-2021',
+                                   "Amsterdam MSM" = 'Other Amsterdam MSM',
+                                   "Non-Amsterdam MSM" = 'Other individuals from outside Amsterdam or a different Amsterdam risk group',
+                                   'NA' = ''),
+                          na.value = "black", drop = F, name = "") +
+      theme(legend.text = element_text(size=11),legend.position='bottom') +
+      guides(col=guide_legend(ncol=1)) +
+      ylim(-1, length(tree$tip.label) +1)
+ggsave(file.path(outdir,glue("type_{subtype}_tree_newlabels_{trsm}_coloursubgraphs_tips_borders_bplace_fan_thicker_lines.pdf")), width = 8, height = tips*0.01, limitsize = F)
 
-## add other data to the plot
-    # Make the original plot
-    #p <- ggtree(tree)
-
-    # generate some random values for each tip label in the data
-    #d1 <- data.frame(id=tree$tip.label, val=rnorm(30, sd=3))
-    d1 <- data.table(tip=tree$tip.label)
-    regex.tip.label <- '^([A-Za-z]+)_+(T[0-9]+)_([0-9]+)_([a-zA-Z]+)_([a-zA-Z]+)_([a-zA-Z]+)-([a-zA-Z]+)_([a-zA-Z_]+)_([0-9]+)-([0-9-]+)-([0-9-]+)_([0-9]+)$'
-    d1[, ID:= as.numeric(gsub(regex.tip.label,'\\3',tip))]
-    d1[, TRSM:= gsub(regex.tip.label,'\\1',tip)]
-    d1 <- merge(d1,unique(subset(pairs,select=c('TO_SEQUENCE_ID','TO_AGE','TO_EST_INFECTION_DATE'))),by.x='ID',by.y='TO_SEQUENCE_ID',all.x=T)
-    d1 <- merge(d1,unique(subset(pairs,select=c('FROM_SEQUENCE_ID','FROM_AGE','FROM_EST_INFECTION_DATE'))),by.x='ID',by.y='FROM_SEQUENCE_ID',all.x=T)
-
-    # Make a second plot with the original, naming the new plot "dot",
-    # using the data you just created, with a point geom.
-    p2 <- facet_plot(tree.display, panel="dot", data=d1, geom=geom_point, aes(x=FROM_AGE2), color='red3',size=0.3)
-    p2 + theme_tree2()
-    ggsave(file.path(outdir,glue("type_{subtype}_tree_newlabels_{trsm}_panel.pdf")), width = 7, height = tips*0.02, limitsize = F)
-
-    # Make some more data with another random value.
-    d2 <- data.frame(id=tree$tip.label, value = abs(rnorm(30, mean=100, sd=50)))
-
-    # Now add to that second plot, this time using the new d2 data above,
-    # This time showing a bar segment, size 3, colored blue.
-    p3 <- facet_plot(p2, panel='bar', data=d2, geom=geom_segment,
-                     aes(x=0, xend=value, y=y, yend=y), size=3, color='blue4')
-
-    # Show all three plots with a scale
-    p3 + theme_tree2()
